9f6f4c8568545aa48a3f07c5d9c3eaf1e54d133b
yoshikawa-mikako <yoshikawa-mikako@outlook.jp>
2016-07-07 10:51:37 +0900

    授業変更 review

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,3 @@
 class Book < ActiveRecord::Base
- validates_length_of :isbn, :maximum => 13
+ has_many :reviews
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160613065209) do
+ActiveRecord::Schema.define(version: 20160706012648) do
 
   create_table "books", force: :cascade do |t|
     t.string   "isbn"
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 20160613065209) do
     t.string   "address"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string   "book_id"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
